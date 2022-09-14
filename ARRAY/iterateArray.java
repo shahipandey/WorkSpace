26f634fd425e130af84174d876f477f1c38c0973
shashipandey <kant72506@gmail.com>
2022-09-14 17:41:19 +0530

    updated

--- ARRAY/iterateArray.java
+++ ARRAY/iterateArray.java
@@ -1,17 +1,39 @@
+import java.nio.IntBuffer;
+
+import javax.sql.rowset.spi.SyncProviderException;
+
 public class iterateArray {
 
-    static void iterate(int arr[]) {
-        for(int i=0; i<arr.length; i++);
+    static int iterate(int arr[], int n) {
+        
+        int sum = 0;
 
-        System.out.println(arr[i]);
 
+        for(int i=0; i<n; i++) {
+
+            sum = sum + arr[i];
+            
+
+        }
+        // System.out.println(sum);
+
+        return sum;
+        
+        
     }
-    // return arr[i];
+        
+    
    
     public static void main(String[] args) {
 
-        // int arr[] = {10, 2, 4, 5, 9, 4, 7}; 
-        iterate(new arr[] {10, 2, 4, 5, 9, 4, 7});
+        
+ 
+        int arr[] = {10, 2, 4, 5, 9, 4, 7 }; 
+        int n = arr.length;
+        // iterate(arr, n);
+
+        System.out.println(iterate(arr, n));
+
     }
 
     
