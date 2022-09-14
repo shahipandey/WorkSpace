f357b0df8f719aafa3dcffe16569a467b3391d13
shashipandey <kant72506@gmail.com>
2022-09-14 16:57:47 +0530

    updated

---
+++ ARRAY/iterateArray.java
@@ -0,0 +1,19 @@
+public class iterateArray {
+
+    static void iterate(int arr[]) {
+        for(int i=0; i<arr.length; i++);
+
+        System.out.println(arr[i]);
+
+    }
+    // return arr[i];
+   
+    public static void main(String[] args) {
+
+        // int arr[] = {10, 2, 4, 5, 9, 4, 7}; 
+        iterate(new arr[] {10, 2, 4, 5, 9, 4, 7});
+    }
+
+    
+}
+
